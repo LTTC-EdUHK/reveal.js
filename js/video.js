f1e2c3e5378f5bee991de9644fae6c3b34851bda
tkingless <t.kingless@yahoo.com>
2019-11-11 08:47:42 +0000

    can flip video

---
+++ js/video.js
@@ -0,0 +1,8 @@
+(function() {
+	"use strict";
+	//var myA = document.querySelector('a#test');
+	var myA = document.querySelector('div#fg_title');
+	console.log(myA);	
+	myA.addEventListener("click", function(){
+	},true);
+})();
