9ea14e9d6b4bf752cb6737e8a054587aec8a60a6
tkingless <t.kingless@yahoo.com>
2019-11-13 09:55:58 +0000

    update transform obj

--- js/video.js
+++ js/video.js
@@ -1,8 +1,27 @@
 (function() {
-	"use strict";
-	//var myA = document.querySelector('a#test');
-	var myA = document.querySelector('div#fg_title');
-	console.log(myA);	
-	myA.addEventListener("click", function(){
-	},true);
+//something ad hoc run here
 })();
+
+function defVideoTag(){
+	var zoomEg = document.querySelector('a#uiZoom'); //the element id cannot have _,- characters
+	var panEg = document.querySelector('a#uiPan'); //the element id cannot have _,- characters
+	var projEg = document.querySelector('a#uiProj'); //the element id cannot have _,- characters
+
+	if(zoomEg != 'null') {
+		console.log('a');
+		zoomEg.addEventListener("click", function(event){
+			event.preventDefault();movie.play();movie.pause();movie.currentTime=13;movie.play();
+		},true);
+	};
+	if(panEg != 'null') {
+		console.log('b');
+		panEg.addEventListener("click", function(event){
+			event.preventDefault();movie.play();movie.pause();movie.currentTime=20;movie.play();
+		},true);
+	};
+	if(projEg != 'null') {
+		console.log('c');
+		projEg.addEventListener("click", function(event){
+			event.preventDefault();movie.play();movie.pause();movie.currentTime=24;movie.play();
+		},true);
+	};}
